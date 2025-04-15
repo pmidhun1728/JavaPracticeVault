4dbca5101f9bd6f98e9905302279ed35338a4300
Midhun <42476012+pmidhun1728@users.noreply.github.com>
2025-04-15 12:46:20 -0400

    Refactored

---
+++ src/test/java/Threads/CreateThread/ExtendThread.java
@@ -0,0 +1,12 @@
+package Threads.CreateThread;
+
+public class ExtendThread extends Thread{
+    public void run() {
+        System.out.println("Thread is Running....");
+    }
+
+    public static void main(String[] args) {
+        ExtendThread thread = new ExtendThread();
+       thread.start();
+    }
+}
---
+++ src/test/java/Threads/CreateThread/ImplementThread.java
@@ -0,0 +1,13 @@
+package Threads.CreateThread;
+
+public class ImplementThread implements Runnable {
+    @Override
+    public void run() {
+        System.out.println("Thread is Running...");
+    }
+
+    public static void main(String[] args) {
+        Thread thread = new Thread(new ImplementThread());
+        thread.start();
+    }
+}
